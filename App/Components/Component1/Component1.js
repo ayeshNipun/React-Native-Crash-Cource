52ac537e50cdf3882634d6f9bd93b8a93ee122c2
ayeshNipun <nipun.yesh@gmail.com>
2019-02-13 21:13:10 +0530

    initialize app

---
+++ App/Components/Component1/Component1.js
@@ -0,0 +1,30 @@
+import React, { Component } from 'react'
+import { AppRegistry, Text, View } from 'react-native'
+
+export default class Component1 extends Component {
+
+	constructor(props) {
+		super(props);
+		this.state = { 
+			name: "Ayesh nipun",
+			age: 21,
+			showName: true,
+			msg: this.props.msg
+		};
+	}
+
+	static defaultProps = {
+		msg: "Hella",
+	}
+
+	render() {
+		return (
+			<View style={{}}>
+				<Text> {this.state.msg} </Text>
+				<Text> {this.state.name} </Text>
+			</View>
+		);
+	}
+}
+
+AppRegistry.registerComponent('Component1', () => Component1);
